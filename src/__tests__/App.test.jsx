ccae0518b0604e199fa5f9568e36e5799c606bb2
Jobet Casquejo <jobetcasquejo99@gmail.com>
2024-03-02 15:39:29 +0000

    added test

---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../App";
+
+test("renders Hello CodeSandbox v2", () => {
+  const { getByText } = render(<App />),
+    linkElement = getByText(/Hello CodeSandbox v2/iu);
+  expect(linkElement).toBeInTheDocument();
+});
